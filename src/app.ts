e9723048eb9370141772d1bf26811c02b4264e3a
Joao Abreu <jp.dellabianca@.gmail.com>
2022-03-05 19:32:36 -0300

    Create middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import "dotenv/config";
 import express from 'express';
+import {Request, Response, NextFunction} from 'express';
 
 import { router } from "./routes";
 
@@ -8,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use((error: Error, req : Request,resp: Response, next: NextFunction) => { 
+    return resp.json({statu: "Error", message: error.message})
+});
 
 // app.get('/github', (req, res) => {
 //     res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
